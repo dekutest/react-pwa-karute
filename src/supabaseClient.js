f055d6dc7a2bfd085e7ef18e0958b696ae19a6a1
ota <78889721+dekutest@users.noreply.github.com>
2025-05-11 23:40:39 +0900

    デプロイ環境の設定完了

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
+// 環境変数からURLとキーを取得
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
+// 環境変数が正しく読み込まれているか確認
+// console.log("Supabase URL:", supabaseUrl);
+// console.log("Supabase Key:", supabaseKey);
+
+// Supabaseクライアントを作成
 export const supabase = createClient(supabaseUrl, supabaseKey);
